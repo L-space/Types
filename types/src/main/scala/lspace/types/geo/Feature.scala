7aa462e982acf8ec5865fe906b93ab59c46c1088
Thijs Broersen <mht.broersen@gmail.com>
2019-09-18 23:25:06 +0200

    empty Map as default for properties

--- types/src/main/scala/lspace/types/geo/Feature.scala
+++ types/src/main/scala/lspace/types/geo/Feature.scala
@@ -1,3 +1,5 @@
 package lspace.types.geo
 
-case class Feature[+T <: Geometry] private (geometry: T, properties: Map[String, Any], bbox: Option[BBox] = None)
+case class Feature[+T <: Geometry] private (geometry: T,
+                                            properties: Map[String, Any] = Map(),
+                                            bbox: Option[BBox] = None)
